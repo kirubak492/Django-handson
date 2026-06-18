83b1fbe2d7bdfc5854b99db4e84ca45c1e0e5a2a
kiruba <kirubak492@gmail.com>
2026-06-18 22:54:24 +0530

    model and migration

--- myapp/settings.py
+++ myapp/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST' : 'localhost',
+        'port' :'3306'
     }
 }
 
