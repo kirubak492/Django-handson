575ea568b9211741453ce2c906a8fe3df2def7e0
kiruba <kirubak492@gmail.com>
2026-06-18 22:25:45 +0530

    logger added

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import logging
 
+logger = logging.getLogger(__name__)
+
+posts=[
+        { 'id':1,'title' : 'Post 1','content':'Content of page 1' },
+        { 'id':2,'title' : 'Post 2','content':'Content of page 2' },
+        { 'id':3,'title' : 'Post 3','content':'Content of page 3' },
+        { 'id':4,'title' : 'Post 4','content':'Content of page 4' },
+    ]
 # Create your views here.
 def home(request):
     return HttpResponse("hello ..welcome to Django")
 
 def index(request):
     title='kiruba blog'
-    posts=[
-        { 'id':1,'title' : 'post 1','content':'content of page 1' },
-        { 'id':2,'title' : 'post 2','content':'content of page 2' },
-        { 'id':3,'title' : 'post 3','content':'content of page 3' },
-        { 'id':4,'title' : 'post 4','content':'content of page 4' },
-    ]
+   
     return render(request,'blog/index.html',{'blog_title':title, 'posts':posts})
 
 def detail(request,post_id):
-    return render(request,'blog/detail.html')
+    post=next((item for item in posts if item['id'] == int(post_id)),None)
+    logger.info(f'post value is {post}')
+    return render(request,'blog/detail.html',{'post':post})
--- myapp/settings.py
+++ myapp/settings.py
@@ -124,3 +124,20 @@ STATICFILES_DIR= [
     "chat/static/blog",
     
 ]
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
