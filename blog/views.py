fc562bcafa6084185d168c8a4aebcf2bff101201
kiruba <kirubak492@gmail.com>
2026-06-18 21:17:57 +0530

    partition and for tag

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,13 @@ def home(request):
 
 def index(request):
     title='kiruba blog'
-    return render(request,'blog/index.html',{'blog_title':title})
+    posts=[
+        { 'title' : 'post 1','content':'content of page 1' },
+        { 'title' : 'post 2','content':'content of page 2' },
+        { 'title' : 'post 3','content':'content of page 3' },
+        { 'title' : 'post 4','content':'content of page 4' },
+    ]
+    return render(request,'blog/index.html',{'blog_title':title, 'posts':posts})
 
 def detail(request,post_id):
     return render(request,'blog/detail.html')
