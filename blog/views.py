7129eee93cabf8e127653396f57402a3bd498d8c
kiruba <kirubak492@gmail.com>
2026-06-18 23:27:48 +0530

    get db data using models

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import logging
-
+from .models import Post
 logger = logging.getLogger(__name__)
 
-posts=[
-        { 'id':1,'title' : 'Post 1','content':'Content of page 1' },
-        { 'id':2,'title' : 'Post 2','content':'Content of page 2' },
-        { 'id':3,'title' : 'Post 3','content':'Content of page 3' },
-        { 'id':4,'title' : 'Post 4','content':'Content of page 4' },
-    ]
+# static post data
+# posts=[
+#         { 'id':1,'title' : 'Post 1','content':'Content of page 1' },
+#         { 'id':2,'title' : 'Post 2','content':'Content of page 2' },
+#         { 'id':3,'title' : 'Post 3','content':'Content of page 3' },
+#         { 'id':4,'title' : 'Post 4','content':'Content of page 4' },
+#     ]
 # Create your views here.
 def home(request):
     return HttpResponse("hello ..welcome to Django")
 
 def index(request):
     title='kiruba blog'
-   
+    posts=Post.objects.all() #get post data using model
     return render(request,'blog/index.html',{'blog_title':title, 'posts':posts})
 
 def detail(request,post_id):
